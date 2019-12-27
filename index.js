ecdbc0011274e008a183356912fb05e7f70ef85a
Ishmet Jahan <ishmetjahan@yahoo.com>
2019-12-26 22:32:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+
+let num1 = 1
+let num2 = 62
+
+let multiply = num1 * num2 
+let random = Math.floor(Math.random()*9)+16
+let mod = 16 %6 
+let max = Math.max (5,10, 15, 20)
